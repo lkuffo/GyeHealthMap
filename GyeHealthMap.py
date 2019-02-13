29f2c3d06233aba342e81f7c880957cca76a5f95
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2019-02-13 02:25:32 -0500

    [Heroku Deploy] Changed app name

--- GyeHealthMap.py
+++ GyeHealthMap.py
@@ -5,7 +5,7 @@ import os
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
 
-app = Flask(__name__)
+app = Flask("GyeHealthMap")
 app.secret_key = 'shhhhhhhh'
 
 @app.route('/')
